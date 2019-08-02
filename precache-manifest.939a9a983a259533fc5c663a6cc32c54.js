32d34f43d67f9b91e343ff9ed73fde636e1ce8a1
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-08-02 12:49:01 +0200

    Updates

---
+++ precache-manifest.939a9a983a259533fc5c663a6cc32c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f5f6f5032a3cf713e25aeecacffdbc",
+    "url": "/react-bootstrap-crud/index.html"
+  },
+  {
+    "revision": "a32b4f704e24c6cb9d50",
+    "url": "/react-bootstrap-crud/static/css/main.311f45db.chunk.css"
+  },
+  {
+    "revision": "bd1d18e729caabac900f",
+    "url": "/react-bootstrap-crud/static/js/2.f69dd98d.chunk.js"
+  },
+  {
+    "revision": "a32b4f704e24c6cb9d50",
+    "url": "/react-bootstrap-crud/static/js/main.bf0ceb86.chunk.js"
+  },
+  {
+    "revision": "f24012ec9ae7222a4a72",
+    "url": "/react-bootstrap-crud/static/js/runtime~main.47f463eb.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-bootstrap-crud/static/media/logo.ee7cd8ed.svg"
+  }
+]);
