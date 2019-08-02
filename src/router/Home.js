a175f53651a17b94dc989014fb8320ee704733af
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-08-02 15:15:42 +0200

    updating home section

--- src/router/Home.js
+++ src/router/Home.js
@@ -11,27 +11,13 @@ export default () => {
           Welcome
         </h1>
         <p className="lead">
-          ilyes zouaoui redux crud is a small project using{" "}
-          <a
-            href="https://redux-starter-kit.js.org/introduction/quick-start"
-            target="_blank"
-          >
-            redux-starter-kit
+          ilyes zouaoui react boostrap crud, is a s mall project using
+          <a href="https://react-bootstrap.github.io/" target="_blank">
+            react-bootstrap{" "}
           </a>{" "}
-          librairy which help us to create redux store, action creator and
-          reducers in a much simpler way then we used to do before using redux
-          library directly, also it includes redux-thunk middleware by default
-          for handling asynchronous operations, and we use{" "}
-          <a
-            href="https://react-redux.js.org/introduction/quick-start"
-            target="_blank"
-          >
-            react-redux
-          </a>{" "}
-          library to be able to connect our react app with our redux solution in
-          a simple way, this project was made for learning how to manage our
-          react application state in a more efficient way using redux, i wish it
-          helps you.
+          library for the styling, and realising a crud operations with it, this
+          project was made for learning how to use react-bootstrap and do a crud
+          operations using local state, i wish it helps you :D .
         </p>
       </div>
     </div>
